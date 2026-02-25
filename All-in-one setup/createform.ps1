84f3c27d7e457f83bcc045c7a7279d3c7f969d46
rschouten97 <69046642+rschouten97@users.noreply.github.com>
2026-02-25 20:49:55 +0100

    Update createform.ps1

--- All-in-one setup/createform.ps1
+++ All-in-one setup/createform.ps1
@@ -21,7 +21,7 @@ $tmpName = @'
 AdUsersEnabledSearchOu
 '@ 
 $tmpValue = @'
-DC=zeeman,DC=com
+DC=enyoi,DC=org
 '@ 
 $globalHelloIDVariables.Add([PSCustomObject]@{name = $tmpName; value = $tmpValue; secret = "False"});
 
